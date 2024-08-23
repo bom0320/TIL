f10b112e6e0d760a239f806993ff923cb46b1df3
bom0320 <167315197+bom0320@users.noreply.github.com>
2024-08-23 10:57:16 +0900

    update: daylist component

---
+++ CSS/Toeic-devil list_day/DayList component.tsx
@@ -0,0 +1,9 @@
+return (
+    <ul className="list_day">
+      {days.map((day) => (
+        <li key={day.id}>
+          <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+        </li>
+      ))}
+    </ul>
+  );
