9abf73385b826f7396a54af6986e20d5283b5e0c
bom0320 <167315197+bom0320@users.noreply.github.com>
2024-09-26 18:28:57 +0900

    update: count code

---
+++ React/code/new Count Code.jsx
@@ -0,0 +1,18 @@
+// 부모 컴포넌트
+const Parent = () => {
+    const [count, setCount] = useState(0);  // 상태는 부모 컴포넌트에서 관리
+  
+    // 상태를 변경하는 함수(콜백)를 자식 컴포넌트에 전달
+    return <Child count={count} increment={() => setCount(count + 1)} />;
+  };
+  
+  // 자식 컴포넌트
+  const Child = ({ count, increment }) => {
+    return (
+      <div>
+        <p>현재 카운트: {count}</p>
+        <button onClick={increment}>카운트 증가</button>
+      </div>
+    );
+  };
+p  
